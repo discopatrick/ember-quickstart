bee2c429c5c6b8c51c9aa4dc00ccec222ccc5135
discopatrick <git@discopatrick.me>
2017-07-30 11:01:47 +0100

    Generate rentals/show route

--- app/router.js
+++ app/router.js
@@ -11,7 +11,9 @@ Router.map(function() {
   this.route('programmers');
   this.route('about');
   this.route('contact');
-  this.route('rentals', function() {});
+  this.route('rentals', function() {
+    this.route('show');
+  });
 });
 
 export default Router;
